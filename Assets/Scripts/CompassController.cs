74c932d2b4dc52b152158fdf08965f1fa0fb6e01
Alexander Tkachenko <alt@alternative.ms>
2021-12-18 14:06:47 +0200

    Logic update. Sprite update. Ready for test on device.

--- Assets/Scripts/CompassController.cs
+++ Assets/Scripts/CompassController.cs
@@ -3,6 +3,9 @@ using UnityEngine.UI;
 
 public sealed class CompassController : MonoBehaviour
 {
+#if UNITY_EDITOR
+    [SerializeField]
+#endif
     private float _rawHeading = 0; // The heading in degrees relative to the geographic North Pole in current frame
     private float _heading = 0;
     private float _deltaHeading = 0;
@@ -15,21 +18,33 @@ public sealed class CompassController : MonoBehaviour
     [SerializeField]
     private Text _textCompassAngle;
 
+    [SerializeField]
+    private bool _smoothValues = false;
+
     private void Start()
     {
+#if !UNITY_EDITOR
         Input.compass.enabled = true;
+#endif
     }
 
     private void Update()
     {
+#if !UNITY_EDITOR
         _rawHeading = Input.compass.trueHeading;
-        _deltaHeading = _heading - _rawHeading;
+#endif
+        if (_smoothValues)
+        {
+            _deltaHeading = _heading - _rawHeading;
 
-        // convert angle from -180..180° to 0..360°
-        if (_deltaHeading > 180) _deltaHeading -= 360; else
-        if (_deltaHeading < -180) _deltaHeading += 360;
+            // convert angle from -180..180° to 0..360°
+            if (_deltaHeading > 180) _deltaHeading -= 360; else if (_deltaHeading < -180) _deltaHeading += 360;
 
-        if (Mathf.Abs(_deltaHeading) > _compassThreshold) _heading = _rawHeading;
+            if (Mathf.Abs(_deltaHeading) > _compassThreshold) _heading = _rawHeading;
+        } else
+        {
+            _heading = _rawHeading;
+        }
 
         UpdateVisualCompass();
     }
@@ -37,5 +52,12 @@ public sealed class CompassController : MonoBehaviour
     private void UpdateVisualCompass()
     {
         _textCompassAngle.text = "" + _heading + "°";
+
+        _imageCompassArrow.rectTransform.localEulerAngles = new Vector3(0, 0, _heading);
+    }
+
+    public void ToggleSmoothValueState(bool newValue)
+    {
+        _smoothValues = newValue;
     }
 }
