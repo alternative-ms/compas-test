0a52d556e2747f9f3a7ffd2022e3619fe4883ff1
Alexander Tkachenko <alt@alternative.ms>
2021-12-18 13:09:56 +0200

    Basic logic added. Ready to test on device.

--- Assets/Scripts/CompassController.cs
+++ Assets/Scripts/CompassController.cs
@@ -1,9 +1,41 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public sealed class CompassController : MonoBehaviour
 {
-    void Update()
+    private float _rawHeading = 0; // The heading in degrees relative to the geographic North Pole in current frame
+    private float _heading = 0;
+    private float _deltaHeading = 0;
+
+    [SerializeField]
+    private float _compassThreshold = 4;
+
+    [SerializeField]
+    private Image _imageCompassArrow;
+    [SerializeField]
+    private Text _textCompassAngle;
+
+    private void Start()
     {
-        // get compass data
+        Input.compass.enabled = true;
+    }
+
+    private void Update()
+    {
+        _rawHeading = Input.compass.trueHeading;
+        _deltaHeading = _heading - _rawHeading;
+
+        // convert angle from -180..180° to 0..360°
+        if (_deltaHeading > 180) _deltaHeading -= 360; else
+        if (_deltaHeading < -180) _deltaHeading += 360;
+
+        if (Mathf.Abs(_deltaHeading) > _compassThreshold) _heading = _rawHeading;
+
+        UpdateVisualCompass();
+    }
+
+    private void UpdateVisualCompass()
+    {
+        _textCompassAngle.text = "" + _heading + "°";
     }
 }
