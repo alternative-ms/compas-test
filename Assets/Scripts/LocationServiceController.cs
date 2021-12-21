a1ddc251c9a8714de9ae8515c89dc22dfc52eba7
Alexander Tkachenko <alt@alternative.ms>
2021-12-21 07:53:27 +0200

    Permission logic hot fix.

--- Assets/Scripts/LocationServiceController.cs
+++ Assets/Scripts/LocationServiceController.cs
@@ -11,21 +11,11 @@ public sealed class LocationServiceController : MonoBehaviour
     private bool _locationServiceStarted = false;
 
     private int _locationServiceTimeoutSeconds = 10;
+
     private void Awake()
     {
         _textDebug.text += "\n" + "Awake()";
-
-        if (!Application.isEditor)
-        {
-            if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
-            {
-                _textDebug.text += "\n" + "RequestUserPermission - FineLocation";
-                Permission.RequestUserPermission(Permission.FineLocation);
-            } else
-            {
-                _textDebug.text += "\n" + "HasUserAuthorizedPermission - FineLocation";
-            }
-        }
+        AskPermission();
     }
 
     private void Start()
@@ -34,13 +24,34 @@ public sealed class LocationServiceController : MonoBehaviour
         StartCoroutine(StartLocationService());
     }
 
+    private void AskPermission()
+    {
+        _textDebug.text += "\n" + "AskPermission()";
+
+        if (!Application.isEditor)
+        {
+            if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+            {
+                _textDebug.text += "\n" + "RequestUserPermission - FineLocation";
+                Permission.RequestUserPermission(Permission.FineLocation);
+            }
+            else
+            {
+                _textDebug.text += "\n" + "HasUserAuthorizedPermission - FineLocation";
+            }
+        }
+    }
+
     private IEnumerator StartLocationService()
     {
         if (!Input.location.isEnabledByUser)
         {
             _textDebug.text += "\n" + "Waiting to start Location Service";
+            AskPermission();
             yield break;
         }
+
+        _textDebug.text += "\n" + "Input.location.Start...";
 
         Input.location.Start(0.1f, 0.1f);
 
@@ -48,6 +59,7 @@ public sealed class LocationServiceController : MonoBehaviour
         {
             yield return new WaitForSeconds(1);
             _locationServiceTimeoutSeconds--;
+            _textDebug.text += "\n" + "Timer :" + _locationServiceTimeoutSeconds;
         }
 
         if (_locationServiceTimeoutSeconds <= 0)
@@ -73,4 +85,12 @@ public sealed class LocationServiceController : MonoBehaviour
     {
 
     }
+
+    /*
+     [debug]
+    Awake()
+    RequestUserPermission - FineLocation
+    Start()
+    Waiting to start Location Service
+     */
 }
