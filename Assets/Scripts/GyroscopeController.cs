0a1a990be268c92cf4f97a58695e84d28582bfa3
Alexander Tkachenko <alt@alternative.ms>
2021-12-22 23:17:07 +0200

    Test 13 - https://youtu.be/h4WNR1vLou8 - Device orientation test
    Added debug info about device orientation via different ways

--- Assets/Scripts/GyroscopeController.cs
+++ Assets/Scripts/GyroscopeController.cs
@@ -37,14 +37,52 @@ public sealed class GyroscopeController : MonoBehaviour
         {
             if (_gyroscopeEnabled)
             {
-                _debugGizmoCube.localRotation = _gyroscope.attitude;
+                _debugGizmoCube.localRotation = _gyroscope.attitude;// * new Quaternion(1, 0, 0, 0); // 0, 0, 1, 0 - v1 | 0, 1, 0, 0 - v2
                 _textGyroscope.text = "Gyroscope : " + "\nX : " + _gyroscope.attitude.x + "\nY : " + _gyroscope.attitude.y + "\nZ : " + _gyroscope.attitude.z + "\nW : " + _gyroscope.attitude.w;
 
-                float angleX = _debugGizmoCube.localEulerAngles.x; /* convert from 0..360° to -180°..180° */ if (angleX > 180) angleX -= 180; if (angleX < -180) angleX += 180;
-                float angleY = _debugGizmoCube.localEulerAngles.y; /* convert from 0..360° to -180°..180° */ if (angleY > 180) angleY -= 180; if (angleY < -180) angleY += 180;
-                float angleZ = _debugGizmoCube.localEulerAngles.z; /* convert from 0..360° to -180°..180° */ if (angleZ > 180) angleZ -= 180; if (angleZ < -180) angleZ += 180;
+                float angleX = _debugGizmoCube.localEulerAngles.x; /* convert from 0..360° to -180°..180° */ if (angleX > 180) angleX -= 360; if (angleX < -180) angleX += 360;
+                float angleY = _debugGizmoCube.localEulerAngles.y; /* convert from 0..360° to -180°..180° */ if (angleY > 180) angleY -= 360; if (angleY < -180) angleY += 360;
+                float angleZ = _debugGizmoCube.localEulerAngles.z; /* convert from 0..360° to -180°..180° */ if (angleZ > 180) angleZ -= 360; if (angleZ < -180) angleZ += 360;
 
                 _textGyroscope.text += "\nGizmo : " + "\nX : " + angleX + "\nY : " + angleY + "\nZ : " + angleZ;
+
+                if ((Mathf.Abs(angleX) >= 50) || (Mathf.Abs(angleY) >= 50))
+                {
+                    _textGyroscope.text += "\nThe device is rotated perpendicular to the ground";
+                } else
+                {
+                    _textGyroscope.text += "\nThe device is parallel to the ground";
+                }
+
+                if (Input.deviceOrientation == DeviceOrientation.Portrait)
+                {
+                    _textGyroscope.text += "\nDeviceOrientation - Portrait";
+                }
+
+                if (Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown)
+                {
+                    _textGyroscope.text += "\nDeviceOrientation - PortraitUpsideDown";
+                }
+
+                if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft)
+                {
+                    _textGyroscope.text += "\nDeviceOrientation - LandscapeLeft";
+                }
+
+                if (Input.deviceOrientation == DeviceOrientation.LandscapeRight)
+                {
+                    _textGyroscope.text += "\nDeviceOrientation - LandscapeRight";
+                }
+
+                if (Input.deviceOrientation == DeviceOrientation.FaceUp)
+                {
+                    _textGyroscope.text += "\nDeviceOrientation - FaceUp";
+                }
+
+                if (Input.deviceOrientation == DeviceOrientation.FaceDown)
+                {
+                    _textGyroscope.text += "\nDeviceOrientation - FaceDown";
+                }
             }
         }
     }
