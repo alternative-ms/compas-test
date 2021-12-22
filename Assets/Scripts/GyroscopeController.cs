60cd59aa1bb19b57a40882d1512ad882f6d88b2a
Alexander Tkachenko <alt@alternative.ms>
2021-12-22 16:11:27 +0200

    Gyroscope on-device test1

---
+++ Assets/Scripts/GyroscopeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GyroscopeController : MonoBehaviour
+{
+    [SerializeField]
+    private TMPro.TextMeshProUGUI _textGyroscope;
+
+    [SerializeField]
+    private Transform _debugGizmoCube;
+
+    private Gyroscope _gyroscope;
+    private Quaternion _debugRotation = new Quaternion(0, 0, 1, 0);
+    private bool _gyroscopeEnabled = false;
+    private void Start()
+    {
+        if (!Application.isEditor)
+        {
+            if (!SystemInfo.supportsGyroscope)
+            {
+                _textGyroscope.text = "Gyroscope not support on this device";
+                _gyroscopeEnabled = false;
+            }
+            else
+            {
+                _gyroscope = Input.gyro;
+                _gyroscope.enabled = true;
+                _gyroscopeEnabled = true;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (!Application.isEditor)
+        {
+            if (_gyroscopeEnabled)
+            {
+                _textGyroscope.text = "Gyroscope : " + "\nX : " + _gyroscope.attitude.x + "\nY : " + _gyroscope.attitude.y + "\nZ : " + _gyroscope.attitude.z + "\nW : " + _gyroscope.attitude.w;
+                _debugGizmoCube.localRotation = _gyroscope.attitude * _debugRotation;
+            }
+        }
+    }
+
+}
