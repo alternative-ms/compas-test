3226f41dea07ca70c33e6532f7a94380374e7f40
Alexander Tkachenko <alt@alternative.ms>
2021-12-23 01:31:33 +0200

    UI update. Ready for compass model toggle.

--- Assets/Scripts/GyroscopeController.cs
+++ Assets/Scripts/GyroscopeController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public sealed class GyroscopeController : MonoBehaviour
 {
+    enum Orientation
+    {
+        parallel,
+        perpendicular,
+    }
+
     [SerializeField]
     private TMPro.TextMeshProUGUI _textGyroscope;
 
-    [SerializeField]
-    private Transform _debugGizmoCube;
-
     private Gyroscope _gyroscope;
     private bool _gyroscopeEnabled = false;
+    private Orientation _previousOrientation = Orientation.parallel;
+    private Orientation _currentOrientation = Orientation.parallel;
 
     private void Start()
     {
@@ -37,12 +42,11 @@ public sealed class GyroscopeController : MonoBehaviour
         {
             if (_gyroscopeEnabled)
             {
-                _debugGizmoCube.localRotation = _gyroscope.attitude;// * new Quaternion(1, 0, 0, 0); // 0, 0, 1, 0 - v1 | 0, 1, 0, 0 - v2
                 _textGyroscope.text = "Gyroscope : " + "\nX : " + _gyroscope.attitude.x + "\nY : " + _gyroscope.attitude.y + "\nZ : " + _gyroscope.attitude.z + "\nW : " + _gyroscope.attitude.w;
 
-                float angleX = _debugGizmoCube.localEulerAngles.x; /* convert from 0..360° to -180°..180° */ if (angleX > 180) angleX -= 360; if (angleX < -180) angleX += 360;
-                float angleY = _debugGizmoCube.localEulerAngles.y; /* convert from 0..360° to -180°..180° */ if (angleY > 180) angleY -= 360; if (angleY < -180) angleY += 360;
-                float angleZ = _debugGizmoCube.localEulerAngles.z; /* convert from 0..360° to -180°..180° */ if (angleZ > 180) angleZ -= 360; if (angleZ < -180) angleZ += 360;
+                float angleX = _gyroscope.attitude.eulerAngles.x; /* convert from 0..360° to -180°..180° */ if (angleX > 180) angleX -= 360; if (angleX < -180) angleX += 360;
+                float angleY = _gyroscope.attitude.eulerAngles.y; /* convert from 0..360° to -180°..180° */ if (angleY > 180) angleY -= 360; if (angleY < -180) angleY += 360;
+                float angleZ = _gyroscope.attitude.eulerAngles.z; /* convert from 0..360° to -180°..180° */ if (angleZ > 180) angleZ -= 360; if (angleZ < -180) angleZ += 360;
 
                 _textGyroscope.text += "\nGizmo : " + "\nX : " + angleX + "\nY : " + angleY + "\nZ : " + angleZ;
 
@@ -86,31 +90,5 @@ public sealed class GyroscopeController : MonoBehaviour
             }
         }
     }
-
-    //on table, portrait, vertical
-    // x -0.007(7..8)
-    // y -0.006(3..5)
-    // z -0.923(4..5)
-    // w 0.38(1..2)
-
-    //on table, portrait, horizontal
-    // x -0.006(2..3)
-    // y -0.006(1..2)
-    // z -0.43(1..2)
-    // w 0.902(1..2)
-
-    //on table, landscape, horizontal
-    // x 0
-    // y 0.009(2..3)
-    // z 0.940(1..2)
-    // w -0.340(3..5)
-
-    //on table, landscape, vertical
-    // x 0.001(7..8)
-    // y 0.009(5..6)
-    // z 0.902(0..1)
-    // w 0.431(5..7)
-
-    // on table - Z axis fron the screen , like cam direction
 
 }
